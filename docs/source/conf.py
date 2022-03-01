cd281a1191826532f333ab4d039f3f832354243e
Artem <molla.artem@gmail.com>
2022-03-01 16:58:37 +0300

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ACME'
+copyright = '2021, ACME'
+author = 'Artem Molla'
 
 release = '0.1'
 version = '0.1.0'
